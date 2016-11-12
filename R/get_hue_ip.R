013b0e02c5f363d20136278d0f80516e04c6f44b
James B <james.black@cantab.net>
2016-11-12 10:52:05 +0100

    get hue bridge ip address

---
+++ R/get_hue_ip.R
@@ -0,0 +1,31 @@
+#' Get the internal IP address of your Hue bridge
+#'
+#' Plot the p-value function for one or two confidence interval pairs.
+#' See following for example of the use in the literature:
+#' Is flutamide effective in patients with bilateral orchiectomy?
+#' Rothman, Kenneth J et al.
+#' The Lancet , Volume 353 , Issue 9159 , 1184
+#'
+#' @section Bugs:
+#' Code repo: \url{https://github.com/epijim/notifyme}
+#'
+#' @keywords R Hue notify
+#' @export
+#' @importFrom magrittr "%>%"
+#' @examples
+#' get_hue_ip()
+
+get_hue_ip <- function(){
+  # IP address of bridge
+  # get IP from discovery service
+  bridge_ip <- readLines(
+    "https://www.meethue.com/api/nupnp",
+    warn = F # not expecting new line
+    ) %>%
+    # remove start
+    gsub('.*internalipaddress":"',"",.) %>%
+    # remove end
+    gsub('\"}]',"",.)
+  message("Your Hue Bridge IP address is: ",bridge_ip)
+  return(bridge_ip)
+}
